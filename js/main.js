644d1e189d57b07f6ff6d004dd1ebccf9e59e65d
lui779 <garcesluis9@gmail.com>
2023-05-08 21:57:38 -0400

    operadores_concatenacion

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ console.log ('¡Hola mundo! Esto es la primera práctica d java Script');
 console.log ('Segunda línea utilizando console.log aunque creo que es una mala práctica. Ya se irá corrigiendo a medida que se aprenda')
 /*Complicado el encontrar las comillas simples en el teclado y también hay que buscarlo. Aquí comentario de varias líneas usando*/
 
+
 let num1 =50;
 let num2 =10;
 let saludo ='¡Hola!';
 let nombreCurso = 'Código LATAM';
 
 let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + 'Esto es' + nombreCurso )
