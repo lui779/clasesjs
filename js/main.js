05827a43e51ed4ad9894047f5cb6257ee1f490a0
lui779 <garcesluis9@gmail.com>
2023-05-08 21:44:24 -0400

    clasesjs

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log ('¡Hola mundo! Esto es la primera práctica d java Script');
+console.log ('Segunda línea utilizando console.log aunque creo que es una mala práctica. Ya se irá corrigiendo a medida que se aprenda')
+/*Complicado el encontrar las comillas simples en el teclado y también hay que buscarlo. Aquí comentario de varias líneas usando*/
+
+let num1 =50;
+let num2 =10;
+let saludo ='¡Hola!';
+let nombreCurso = 'Código LATAM';
+
+let resultadoSuma = num1 + num2;
